f890e9a8651fd107af2cbe6004562b1991251bd3
Opilane TTHK <opilane@tthk.ee>
2021-11-09 15:40:45 +0200

    Horoscope

---
+++ Colours/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your colour:");
+            string userSign = Console.ReadLine();
+
+            Console.WriteLine(GetColour(userSign));
+
+        }
+
+        public static string[] ReadColour()
+        {
+            string filePath = @$"C:\Users\opilane\samples\personality.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+
+        public static string GetColour(string userInput)
+        {
+            string[] colour = ReadColour();
+            string todayColour = "";
+
+            foreach (string element in colour)
+            {
+                if (element.Contains(userInput))
+                {
+                    todayColour = element;
+                }
+            }
+
+            return todayColour;
+        }
+
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your horoscope sign:");
+            string userSign = Console.ReadLine();
+
+            Console.WriteLine(GetHoroscope(userSign));
+
+        }
+
+        public static string[] ReadHoroScope()
+        {
+            string filePath = @$"C:\Users\opilane\samples\horoscope.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+
+        public static string GetHoroscope(string userInput)
+        {
+            string[] horoscope = ReadHoroScope();
+            string todayHoroscope = "";
+
+            foreach(string element in horoscope)
+            {
+                if(element.Contains(userInput))
+                {
+                    todayHoroscope = element;
+                }
+            }
+
+            return todayHoroscope;
+        }
+
+    }
+}
